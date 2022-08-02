da7c36ca181eaaeec44fb594ee7ef63c09b83cdd
pharned <harned.exchange@gmail.com>
2022-08-01 20:25:25 -0500

    jkjkjjl

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,9 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
+  photosTitle = 'Nobody Look!';
   image1 = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
-  image2 = '';
+  image2 = 'https://m.media-amazon.com/images/I/61bD-ldvEIL._AC_SL1000_.jpg';
   image3 = '';
 
   constructor() { }
